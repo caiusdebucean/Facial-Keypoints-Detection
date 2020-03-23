1642160ab3a7243c7777e6a408ba5a86231ec7d3
caiusdebucean <debuceancaius@gmail.com>
2020-03-23 22:19:28 +0200

    Added network architecture Version 0

---
+++ network.py
@@ -0,0 +1,12 @@
+import matplotlib.pyplot as plt
+import numpy as np
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+
+## TODO: Once you've define the network, you can instantiate it
+# one example conv layer has been provided for you
+from models import Net
+
+net = Net()
+print(net)
